f142fbcd034ba2f0737b0f4e752212a7951758b4
m-tsuru <147146974+m-tsuru@users.noreply.github.com>
2024-08-05 11:14:01 +0900

    ToDoアプリ -> データの追加処理(Create)に対応する
    https://iij.github.io/bootcamp/frontend/react/#文字列のstateとonchange

--- src/App.tsx
+++ src/App.tsx
@@ -4,32 +4,82 @@ export type TodoItem = {
   id: number;
   text: string;
   done: boolean;
-}
+};
 
 type TodoListItemProps = {
   item: TodoItem;
+  onCheck: (checked: boolean) => void;
+};
+
+type ValueViewerProps = {
+  value: any;
+};
+
+type CreateTodoFormProps = {
+  onSubmit: (text: string) => void;
+};
+
+function CreateTodoForm({ onSubmit }: CreateTodoFormProps) {
+  const [text, setText] = useState("");
+  return (
+    <div className="CreateTodoForm">
+      <input
+        type="text"
+        placeholder="New ToDo"
+        size={60}
+        value={text}
+        onChange={(event) => setText(event.currentTarget.value)}
+      />
+      <button onClick={() => onSubmit(text)}>Add</button>
+    </div>
+  );
 }
 
-function TodoListItem({ item }: TodoListItemProps) {
+function TodoListItem({ item, onCheck }: TodoListItemProps) {
   return (
     <div className="TodoItem">
-      <p style = {{ textDecoration: item.done ? "line-through" : "none" }}>
-        { item.text }
+      <input
+        type="checkbox"
+        checked={item.done}
+        onChange={(event) => onCheck(event.currentTarget.checked)}
+      />
+      <p style={{ textDecoration: item.done ? "line-through" : "none" }}>
+        {item.text}
       </p>
     </div>
   );
 }
 
+function ValueViewer({ value }: ValueViewerProps) {
+  return (
+    <pre className="ValueViewer">{JSON.stringify(value, undefined, 2)}</pre>
+  );
+}
+
 const INITIAL_TODO: TodoItem[] = [
-  {id: 1, text: "todo-item-1", done: false},
-  {id: 2, text: "todo-item-2", done: true}
-]
+  { id: 1, text: "todo-item-1", done: false },
+  { id: 2, text: "todo-item-2", done: true },
+];
+
+const generateId = () => Date.now();
 
 export default function App() {
-  const todoItems = INITIAL_TODO;
+  const [todoItems, setTodoItems] = useState(INITIAL_TODO);
   const [keyword, setKeyword] = useState<string>("");
+  const [showingDone, setShowingDone] = useState<boolean>(true);
+
+  const createItem = (text: string) => {
+    setTodoItems([...todoItems, { id: generateId(), text, done: false }]);
+  };
+
+  const updateItem = (newItem: TodoItem) => {
+    setTodoItems(
+      todoItems.map((item) => (item.id === newItem.id ? newItem : item))
+    );
+  };
 
   const filteredTodoItems = todoItems.filter((item) => {
+    if (!showingDone && item.done) return false;
     return item.text.includes(keyword);
   });
 
@@ -39,20 +89,35 @@ export default function App() {
         <h1>ToDo</h1>
         <input
           type="text"
-          placeholder="Filter" 
-          value={keyword} 
+          placeholder="Filter"
+          value={keyword}
           onChange={(event) => setKeyword(event.target.value)}
         />
+        <input
+          id="showing-done"
+          type="checkbox"
+          checked={showingDone}
+          onChange={(event) => setShowingDone(event.target.checked)}
+        />
+        <label htmlFor="showing-done">Show Finished ToDos</label>
       </div>
-    { filteredTodoItems.length === 0 ? (
-      <div className="dimmed">There is no TODO</div>
-    ) : (
-      <div className="App_todo-list">
-        {filteredTodoItems.map((item) => (
-          <TodoListItem key={item.id} item={item} />
-        ))}
-      </div>
-    )}
-  </div>
-  )
+      {filteredTodoItems.length === 0 ? (
+        <div className="dimmed">There is no TODO</div>
+      ) : (
+        <div className="App_todo-list">
+          {filteredTodoItems.map((item) => (
+            <TodoListItem
+              key={item.id}
+              item={item}
+              onCheck={(checked) => {
+                updateItem({ ...item, done: checked });
+              }}
+            />
+          ))}
+        </div>
+      )}
+      <CreateTodoForm onSubmit={(text) => createItem(text)} />
+      <ValueViewer value={{ keyword, todoItems, filteredTodoItems }} />
+    </div>
+  );
 }
